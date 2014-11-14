464d265b981af9a06e73e1c645ce4e8bdbd41f4c
Mu Dengke <mudengke@gmail.com>
2014-11-14 23:21:29 +0800

    Add testng

--- src/com/test/webservice/Learn1.java
+++ src/com/test/webservice/Learn1.java
@@ -4,15 +4,41 @@ package com.test.webservice;
  * Created by Mu on 2014/11/13.
  */
 
-import static com.jayway.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import com.jayway.restassured.builder.ResponseSpecBuilder;
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.jayway.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.is;
 
 public class Learn1 {
-    public static void main(String[] args) {
-        System.out.println("test");
-        get("http://www.weather.com.cn/data/cityinfo/101010100.html").then().body("weatherinfo.cityid", equalTo(101010100));
-        System.out.println("test");
 
+    String url = null;
+
+    @BeforeClass
+    public void setUp(){
+        url = "http://www.weather.com.cn/data/cityinfo/101270101.html";
+    }
+
+    @Test(groups = { "json" })
+    public void chengdu() {
+        ResponseSpecBuilder builder = new ResponseSpecBuilder();
+        builder.expectStatusCode(200);
+        builder.expectBody("weatherinfo.cityid", is(101270101));
+        ResponseSpecification responseSpec = builder.build();
+
+        get(url).then().spec(responseSpec);
+
+    }
+
+    public static void main(String[] args) {
+        String json = get("http://www.weather.com.cn/data/cityinfo/101270101.html").asString();
+        JsonPath jsonPath = new JsonPath(json).setRoot("weatherinfo");
+        jsonPath.prettyPrint();
+        int cityId = jsonPath.getInt("cityid");
+        System.out.println(cityId);
     }
 }
 
